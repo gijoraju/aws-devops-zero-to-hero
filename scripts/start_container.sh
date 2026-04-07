ddab11166d839a90a9568fa602097dd2a6c72935
GIJO RAJU <65697049+gijoraju@users.noreply.github.com>
2026-04-07 14:55:22 -0400

    changes5

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -4,5 +4,9 @@ set -e
 # Pull the Docker image from Docker Hub
 docker pull rajugijo/simple-python-flask-app:latest
 
+# Stop and remove existing container
+docker stop simple-python-flask-app || true
+docker rm simple-python-flask-app || true
+
 # Run the Docker image as a container
 docker run -d -p 5000:5000 rajugijo/simple-python-flask-app:latest
